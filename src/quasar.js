a23f4e4a8e8b75a71ec944fbd2256ffc1ff47eb7
Colin Brayton <gringolalia@gmail.com>
2019-09-19 18:58:26 -0300

    queasy quasars

---
+++ src/quasar.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+
+import './styles/quasar.styl'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import '@quasar/extras/ionicons-v4/ionicons-v4.css'
+import '@quasar/extras/mdi-v3/mdi-v3.css'
+import Quasar from 'quasar'
+
+Vue.use(Quasar, {
+  config: {}
+ })
